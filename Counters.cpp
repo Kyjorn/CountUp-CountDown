6a43c029643890c482b4c047a3c13b151c9c57b6
Kyjron <hendonmshane@gmail.com>
2025-05-04 17:08:02 -0600

    Added countUp method

---
+++ Counters.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <thread>
+using namespace std;
+
+//counts from 1 - 20
+void countUp() {
+    cout << "Counting Up..." << endl;
+    for (int i = 1; i <= 20; i++){
+        cout << i << endl;
+    }
+}
+
+void countDown() {
+
+}
+
+int main() {
+
+    return 0;
+}
